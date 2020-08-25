7b1036d953298701c8af5d79293a60e639954a5b
Christi15 <Christiwhite1@protonmail.com>
2020-08-25 21:26:16 +0000

    Build files for GitHub pages

---
+++ precache-manifest.7e2b0f1e7a9aa3924974cc667acdfe93.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c282d81f22a54fb08838a31116eb8a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "b426c0f60be924bb48f7",
+    "url": "/resume/static/css/2.ffdfee16.chunk.css"
+  },
+  {
+    "revision": "16b78e1760672a9e6590",
+    "url": "/resume/static/css/4.13fe2b0c.chunk.css"
+  },
+  {
+    "revision": "8b6dfb236dc1580b6f0e",
+    "url": "/resume/static/css/main.46399a82.chunk.css"
+  },
+  {
+    "revision": "b426c0f60be924bb48f7",
+    "url": "/resume/static/js/2.9faf3d3c.chunk.js"
+  },
+  {
+    "revision": "573c03b54bf739ab0b9029188d0efb17",
+    "url": "/resume/static/js/2.9faf3d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fe30d0cf8c603083b5",
+    "url": "/resume/static/js/3.9e35701a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/resume/static/js/3.9e35701a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b78e1760672a9e6590",
+    "url": "/resume/static/js/4.ae4f2300.chunk.js"
+  },
+  {
+    "revision": "8b6dfb236dc1580b6f0e",
+    "url": "/resume/static/js/main.df14698e.chunk.js"
+  },
+  {
+    "revision": "0fec7746dc6870cc2936",
+    "url": "/resume/static/js/runtime-main.662a4c8d.js"
+  },
+  {
+    "revision": "3e7b4ed42120fe2c30460e8ad09f7070",
+    "url": "/resume/static/media/avatar.3e7b4ed4.jpeg"
+  },
+  {
+    "revision": "6ca08aa00e77629f7a6a2ce5e412992c",
+    "url": "/resume/static/media/avatar.6ca08aa0.jpg"
+  },
+  {
+    "revision": "11ee4bf8e3a5cb2a05bebc97d9ac9ffc",
+    "url": "/resume/static/media/avatars-000597831615-6q438f-t500x500.11ee4bf8.jpg"
+  },
+  {
+    "revision": "f971dd94f62c4662e732ea74d2561260",
+    "url": "/resume/static/media/ayin_qoph.f971dd94.png"
+  },
+  {
+    "revision": "aea271b930d57b95ca626bb1a9fb27ee",
+    "url": "/resume/static/media/contact.aea271b9.svg"
+  },
+  {
+    "revision": "075fe2c07825b280a4a6c902e7e06631",
+    "url": "/resume/static/media/defaultAvatar.075fe2c0.svg"
+  },
+  {
+    "revision": "1976f27fa4b3f0b14b524d0f86f0aee2",
+    "url": "/resume/static/media/dummy-project.1976f27f.png"
+  },
+  {
+    "revision": "9aece68254c95eca4a0875e213e1d521",
+    "url": "/resume/static/media/focused_working.9aece682.svg"
+  }
+]);
